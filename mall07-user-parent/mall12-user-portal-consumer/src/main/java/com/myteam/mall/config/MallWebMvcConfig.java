3193e63f5af01d36c91a1c4a0f3e3111d406a083
js6288 <linjs6288@gmail.com>
2021-10-04 10:49:29 +0800

    创建viewController

--- mall07-user-parent/mall12-user-portal-consumer/src/main/java/com/myteam/mall/config/MallWebMvcConfig.java
+++ mall07-user-parent/mall12-user-portal-consumer/src/main/java/com/myteam/mall/config/MallWebMvcConfig.java
@@ -1,7 +1,27 @@
 package com.myteam.mall.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author mzx
+ */
+@Configuration
 public class MallWebMvcConfig implements WebMvcConfigurer {
 
+    /**
+     *
+     * 类似于spring-mvc配置文件的
+     * <mvc:view-controller path="/portal/user/to/reg/page.html" view-name="user-reg"/>
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        //浏览器访问的地址
+        String urlPath = "/portal/user/to/reg/page.html";
+        //目标试图的名称
+        String viewName = "user-reg";
+        registry.addViewController(urlPath).setViewName(viewName);
+    }
 }
